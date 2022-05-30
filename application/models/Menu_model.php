63e7a2252235e3cca612ddef98fe8fd1873f3d27
inadayutaka0707 <74123404+inadayutaka0707@users.noreply.github.com>
2022-05-30 21:19:37 +0900

    Revert "商品一覧表示まで完成"

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -3,27 +3,5 @@ class Menu_model extends CI_Model
 {
 	public function __construct()
 	{
-		$this->load->database();
-	}
-
-	public function get_items()
-	{
-		$sql = 'SELECT id,name,description,price_m,price_l,image_path FROM items;';
-		$result = $this->db->query($sql);
-		return $result->result_array();
-	}
-
-	public function get_detail_item($id)
-	{
-		$sql = 'SELECT id,name,description,price_m,price_l,image_path FROM items WHERE id=?;';
-		$result = $this->db->query($sql, $id);
-		return $result->row_array();
-	}
-
-	public function get_toppings()
-	{
-		$sql = 'SELECT id,name,price_m,price_l FROM toppings;';
-		$result = $this->db->query($sql);
-		return $result->result_array();
 	}
 }
